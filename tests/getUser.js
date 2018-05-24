97ae41c9a847bb1e3c3079fceafc434fff21b939
climandrei94 <clim_andrei2000@yahoo.com>
2018-05-24 13:42:58 +0300

    Added test

--- tests/getUser.js
+++ tests/getUser.js
@@ -1,26 +1,56 @@
-var axios = require("axios");
-const { expect } = require("chai");
-
-let getUser = (id) => {
-    return axios
-        .get(`http://localhost:3000/users/${id}`)
-        .then(res => res.data)
-        .catch(error => console.log(error));
-}
-
 var example = require("../db/users.json")[0];
-var throwError = {
+var errorMessage = {
     "error": "User not found"
 }
 
-describe("GetUser Module", () => {
-    it("Should return user with id 0", () => {
-        return getUser(0)
-            .then(res => {
-                expect(res).to.deep.equal(example)
+const chai = require('chai');
+const chaiHttp = require('chai-http')
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('GetUser Module', () => {
+    it("Should return user with id 0", (done) => {
+        chai.request(`http://localhost:3000`)
+            .get('/users/0')
+            .end((error, response) => {
+                response.should.have.status(200);
+                response.should.be.json;
+                chai.expect(response.body).to.deep.equal(example);
+                done();
             })
     });
 
-})
+    it("Should return error 404 for id -1", (done) => {
+        chai.request(`http://localhost:3000`)
+            .get('/users/-1')
+            .end((error, response) => {
+                response.should.have.status(404);
+                response.should.be.json;
+                chai.expect(response.body).to.deep.equal(errorMessage);
+                done();
+            })
+    });
 
+    it("Should return error 404 for id 'A'", (done) => {
+        chai.request(`http://localhost:3000`)
+            .get('/users/A')
+            .end((error, response) => {
+                response.should.have.status(404);
+                response.should.be.json;
+                chai.expect(response.body).to.deep.equal(errorMessage);
+                done();
+            })
+    });
+
+    it("Should return error 404 for id '0'", (done) => {
+        chai.request(`http://localhost:3000`)
+            .get('/users/"0"')
+            .end((error, response) => {
+                response.should.have.status(404);
+                response.should.be.json;
+                chai.expect(response.body).to.deep.equal(errorMessage);
+                done();
+            })
+    });
+})
 
