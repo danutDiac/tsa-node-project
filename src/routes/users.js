77ee32d72515b867c6d563457ee3e8a450607593
iosifboanca <33721507+iosifboanca@users.noreply.github.com>
2018-05-28 13:47:41 +0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,6 @@
-let getUser = require("../actions/userActions");
-let createUser = require("../actions/createUser");
+let createUser = require("../actions/createUser").createUser;
 let router = require("express").Router();
 
-
-router.get("/:id", getUser);
 router.post("/", createUser);
 
 module.exports = router;
