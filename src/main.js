9f92b4c19e1222cb75a821b68617b4b05a42406f
Andrei Cioromila <andrei.cioromila@thinslices.com>
2018-05-29 16:41:30 +0300

    Add unnecessarily deleted files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 let express = require("express");
 let bodyParser = require("body-parser")
 let usersRouter = require("./routes/users");
+let daysRouter = require("./routes/days");
 
 let app = express();
 app.use(bodyParser.json());
+app.use("/days", daysRouter);
 app.use("/users", usersRouter);
 
-
 app.listen(3000);
-
