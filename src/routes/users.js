af30fb7918c5d0be82aac6e78cb112390e96fd41
iosifboanca <33721507+iosifboanca@users.noreply.github.com>
2018-05-30 17:28:52 +0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,14 @@
 let { createUser } = require("../actions/createUser");
 let { getUser } = require("../actions/getUsers");
+let {retrieveUsers} = require("../actions/retrieveUsers");
+let { deleteUser } = require('../actions/deleteUserActions')
 
 let router = require("express").Router();
 let fs = require('fs')
-let { deleteUser } = require('../actions/deleteUserActions')
 
 router.post("/", createUser);
 router.get("/:id", getUser);
+router.get("/retrieve/:name",retrieveUsers);
 router.delete('/:id', deleteUser)
 
 module.exports = router;
