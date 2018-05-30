5e4b49e558bf7bd8c03708596d52bda042a1ab34
h-mihail <33.mihail@gmail.com>
2018-05-30 12:07:27 +0300

    morgan logging

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+let fs = require('fs')
 let express = require("express");
 let bodyParser = require("body-parser")
+let morgan = require('morgan')
 let usersRouter = require("./routes/users");
 let daysRouter = require("./routes/days");
 let exampleRouter = require("./routes/example");
@@ -9,8 +11,12 @@ app.use(bodyParser.json());
 app.use("/days", daysRouter);
 app.use("/users", usersRouter);
 app.use("/api", exampleRouter);
+app.use(morgan('[:date[clf]] :method ":url" :status', {
+    skip: (req, res) => res.statusCode < 400,
+    stream: fs.createWriteStream('logs/errors.log', { flags: 'a' })
+}))
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('Server started on localhost:3000')
 });
 
