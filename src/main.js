2aaec6337cfaa5e277056f13b7679c5a4bf049c0
h-mihail <33.mihail@gmail.com>
2018-05-30 18:24:33 +0300

    timezone and request body

--- src/main.js
+++ src/main.js
@@ -2,19 +2,24 @@ let fs = require('fs')
 let express = require("express");
 let bodyParser = require("body-parser")
 let morgan = require('morgan')
+let moment = require('moment-timezone');
 let usersRouter = require("./routes/users");
 let daysRouter = require("./routes/days");
 let exampleRouter = require("./routes/example");
 
 let app = express();
+
+morgan.token('date', (req, res, tz) => moment().tz(tz).format())
+morgan.token('res', (req, res, field) => res[field])
+morgan.token('req', (req, res, field) => JSON.stringify(req[field], null, 2))
+morgan.format('dataFormat', '[:date[Europe/Bucharest]] :method ":url" :status :res[statusMessage] - :req[body]')
+const stream = fs.createWriteStream('logs/errors.log', { flags: 'a' })
+const skip = (req, res) => res.statusCode < 400
+app.use(morgan('dataFormat', { skip, stream }))
 app.use(bodyParser.json());
 app.use("/days", daysRouter);
 app.use("/users", usersRouter);
 app.use("/api", exampleRouter);
-app.use(morgan('[:date[clf]] :method ":url" :status', {
-    skip: (req, res) => res.statusCode < 400,
-    stream: fs.createWriteStream('logs/errors.log', { flags: 'a' })
-}))
 
 app.listen(3000, function () {
     console.log('Server started on localhost:3000')
