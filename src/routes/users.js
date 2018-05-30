6f4a176887a0129e5a9dc2fd2f98616c4fcc980d
ancai0ana <1m3myl0v3A>
2018-05-30 14:32:39 +0300

    edit user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 let { createUser } = require("../actions/createUser");
 let { getUser } = require("../actions/getUsers");
+let { editUserPut, editUserPatch } = require("../actions/editUser")
 
 let router = require("express").Router();
 let fs = require('fs')
@@ -8,5 +9,7 @@ let { deleteUser } = require('../actions/deleteUserActions')
 router.post("/", createUser);
 router.get("/:id", getUser);
 router.delete('/:id', deleteUser)
+router.patch("/:id", editUserPatch)
+router.put("/:id", editUserPut)
 
 module.exports = router;
