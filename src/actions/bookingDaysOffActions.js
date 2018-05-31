8404389568138cd9e7a53a5cb4c4e635fbb9b9be
climandrei94 <clim_andrei2000@yahoo.com>
2018-05-31 17:28:18 +0300

    Refactored BookingDaysOff module

--- src/actions/bookingDaysOffActions.js
+++ src/actions/bookingDaysOffActions.js
@@ -7,101 +7,151 @@ const {
 } = require("../helpers/helpers");
 
 const isDateValid = date => {
-    const regex = /([2][0-9]{3})(-)([1][0-2]|[0][1-9])(-)([0][1-9]|[1-2][0-9]|([3][0-1]))/;
-    return date !== undefined && typeof date === "string" && regex.test(date);
-};
+    const regex = /([0-9]{4})(-)([1][0-2]|[0][1-9])(-)([0][1-9]|[1-2][0-9]|([3][0-1]))/;
+    return date !== undefined && typeof date === "string" && regex.test(date) && (new Date(date).getFullYear()) <= (new Date().getFullYear() + 3);
+}
+
+const validateBody = (body) => {
+    return new Promise((resolve, reject) => {
+        let rejectStatus = ""
+        if (Number.isNaN(Number(body["userId"]))) {
+            rejectStatus += "User id is not a number\n"
+        }
+        if (!isDateValid(body["startDate"])) {
+            rejectStatus += "Start date is invalid\n";
+        }
+        if (!isDateValid(body["endDate"])) {
+            rejectStatus += "End date is invalid\n";
+        }
+        if (rejectStatus !== "") reject({
+            "status": 400,
+            "message": rejectStatus
+        });
+        resolve();
+    });
+}
+
+const parseJSON = (data) => {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(JSON.parse(data));
+        }
+        catch (error) {
+            reject({
+                "status": 500,
+                "message": error
+            });
+        }
+    })
+}
+
+const validateUserId = (users, id) => {
+    return new Promise((resolve, reject) => {
+        if (findItemById(users, id) === undefined) {
+            reject({
+                "status": 404,
+                "message": "User not found"
+            });
+        }
+        resolve();
+    })
+}
+
+const formatDate = date => {
+    if (date < 10) return `0${date}`;
+    else return String(date);
+}
 
 const createDaysOffArray = (startDate, endDate) => {
-    let daysOffArray = [];
-    let month = "";
-    let day = "";
+    return new Promise((resolve, reject) => {
+        startDate = new Date(startDate);
+        endDate = new Date(endDate);
 
-    if (startDate > endDate) return [];
+        let daysOffArray = [];
+        let month = "";
+        let day = "";
 
-    endDate.setDate(endDate.getDate() + 1);
-    for (; startDate < endDate; startDate.setDate(startDate.getDate() + 1)) {
-        if (startDate.getDay() > 0 && startDate.getDay() < 6) {
-            if (startDate.getUTCMonth() + 1 < 10) {
-                month = `0${startDate.getUTCMonth() + 1}`;
-            } else {
-                month = `${startDate.getUTCMonth() + 1}`;
-            }
-            if (startDate.getDate() < 10) {
-                day = `0${startDate.getDate()}`;
-            } else {
-                day = `${startDate.getDate()}`;
-            }
-            daysOffArray.push(`${startDate.getFullYear()}-${month}-${day}`);
+        if (startDate > endDate) {
+            reject({
+                "status": 400,
+                "message": "End date lower than start date"
+            });
         }
-    }
 
-    return daysOffArray;
-};
+        endDate.setDate(endDate.getDate() + 1);
+        for (; startDate < endDate; startDate.setDate(startDate.getDate() + 1)) {
+            if (startDate.getDay() > 0 && startDate.getDay() < 6) {
+                daysOffArray.push(`${startDate.getFullYear()}-${formatDate(startDate.getUTCMonth() + 1)}-${formatDate(startDate.getDate())}`);
+            }
+        }
+        
+        resolve(daysOffArray);
+    })
+}
 
 const createNewJson = (daysOff, body, daysOffArray) => {
-    return {
-        id: newId(daysOff),
-        userId: body["userId"],
-        daysOff: daysOffArray
-    };
+    return new Promise((resolve, reject) => {
+        resolve({
+            id: newId(daysOff),
+            userId: body["userId"],
+            daysOff: daysOffArray
+        });
+    })
+}
+
+const addNewJsonInArray = (daysOff, newJson) => {
+    return new Promise((resolve, reject) => {
+        daysOff.push(newJson);
+        resolve(daysOff);
+    })
 }
 
 const bookDaysOff = (request, response) => {
-    readFile("db/users.json")
-    .then(data => {
-        const users = JSON.parse(data);
-        const body = request["body"];
+    let daysOffArray = undefined;
+    let newJson = undefined;
+    let daysOff = undefined;
 
-        if (findItemById(users, Number(body["userId"])) !== undefined) {
-            readFile("db/daysOff.json")
-            .then(data => {
-                let daysOff = JSON.parse(data);
-                let daysOffArray = [];
-        
-                if (
-                    isDateValid(body["startDate"]) &&
-                    isDateValid(body["endDate"])
-                ) {
-                    const startDate = new Date(body["startDate"]);
-                    const endDate = new Date(body["endDate"]);
-                    daysOffArray = createDaysOffArray(startDate, endDate);
-            
-                    const newJson = createNewJson(daysOff, body, daysOffArray);
-                    daysOff.push(newJson);
-                    
-                    writeFile("db/daysOff.json", JSON.stringify(daysOff))
-                    .then(() => {
-                        response.status(200).send(`/days/${newJson["id"]}`);
-                    })
-                    .catch(error => {
-                        response.status(500).json({
-                            serverErrorMessage: "the error was logged and we’ll be checking it shortly"
-                        });
-                    });
-                } else {
-                    response.status(400).json({
-                        error: "Bad request"
-                    });
-                }
-            })
-            .catch(error => {
-                response.status(500).json({
-                    serverErrorMessage: "the error was logged and we’ll be checking it shortly"
-                });
-            })
-        } else {
-        response.status(400).json({
-            error: "Bad request"
-        });
-    }
-    })
-    .catch(error => {
-        response.status(500).json({
-            serverErrorMessage: "the error was logged and we’ll be checking it shortly"
-        });
-    })
-
-};
+    validateBody(request.body)
+        .then(() => {
+            return readFile("db/users.json");
+        })
+        .then(parseJSON)
+        .then((users) => {
+            return validateUserId(users, Number(request.body["userId"]))
+        })
+        .then(() => {
+            return createDaysOffArray(request.body["startDate"], request.body["endDate"])
+        })
+        .then((createdDaysOffArray) => {
+            daysOffArray = createdDaysOffArray;
+            return readFile("db/daysOff.json")
+        })
+        .then(parseJSON)
+        .then((createdDaysOff) => {
+            daysOff = createdDaysOff;
+            return createNewJson(daysOff, request.body, daysOffArray)
+        })
+        .then((createdJson) => {
+            newJson = createdJson;
+            return addNewJsonInArray(daysOff, newJson);
+        })
+        .then((daysOff) => {
+            return writeFile("db/daysOff.json", JSON.stringify(daysOff));
+        })
+        .then(() => {
+            response.status(200).json({
+                "GET": `http://localhost:3000/days/${newJson["id"]}`,
+                "PATCH": `http://localhost:3000/days/${newJson["id"]}`,
+                "PUT": `http://localhost:3000/days/${newJson["id"]}`,
+                "DELETE": `http://localhost:3000/days/${newJson["id"]}`
+            });
+        })
+        .catch((error) => {
+            response.status(error["status"]).json({
+                "error": error["message"]
+            });
+        })
+}
 
 module.exports = {
     isDateValid,
--- tests/bookDaysOffActions.test.js
+++ tests/bookDaysOffActions.test.js
@@ -44,54 +44,61 @@ describe('Booking days module', () => {
 
     it("Should log days correctly for startdate = 2018-10-20 and enddate = 2018-10-30", (done) => {
         const { createDaysOffArray } = require('../src/actions/bookingDaysOffActions');
-        const startDate = new Date("2018-10-20");
-        const endDate = new Date("2018-10-30");
-        const input = createDaysOffArray(startDate, endDate);
-        const result = [
-            "2018-10-22",
-            "2018-10-23",
-            "2018-10-24",
-            "2018-10-25",
-            "2018-10-26",
-            "2018-10-29",
-            "2018-10-30"
-        ];
-
-        chai.expect(input).to.deep.equal(result);
-        done();
+        const startDate = "2018-10-20";
+        const endDate = "2018-10-30";
+        createDaysOffArray(startDate, endDate)
+        .then(input => {
+            const result = [
+                "2018-10-22",
+                "2018-10-23",
+                "2018-10-24",
+                "2018-10-25",
+                "2018-10-26",
+                "2018-10-29",
+                "2018-10-30"
+            ];
+    
+            chai.expect(input).to.deep.equal(result);
+            done();
+        })
     })
-
+    
     it("Should log days correctly for startdate = 2018-10-20 and enddate = 2018-11-04", (done) => {
         const { createDaysOffArray } = require('../src/actions/bookingDaysOffActions');
-        const startDate = new Date("2018-10-20");
-        const endDate = new Date("2018-11-04");
-        const input = createDaysOffArray(startDate, endDate);
-        const result = [
-            "2018-10-22",
-            "2018-10-23",
-            "2018-10-24",
-            "2018-10-25",
-            "2018-10-26",
-            "2018-10-29",
-            "2018-10-30",
-            "2018-10-31",
-            "2018-11-01",
-            "2018-11-02",
-        ];
-
-        chai.expect(input).to.deep.equal(result);
-        done();
+        const startDate = "2018-10-20";
+        const endDate = "2018-11-04";
+        createDaysOffArray(startDate, endDate)
+        .then(input => {
+            const result = [
+                "2018-10-22",
+                "2018-10-23",
+                "2018-10-24",
+                "2018-10-25",
+                "2018-10-26",
+                "2018-10-29",
+                "2018-10-30",
+                "2018-10-31",
+                "2018-11-01",
+                "2018-11-02",
+            ];
+    
+            chai.expect(input).to.deep.equal(result);
+            done();
+        })
+        .catch(error => {
+            console.log(error);
+        });
     })
 
     it("Should return [] for startdate = 2018-10-20 and enddate = 2018-10-10", (done) => {
         const { createDaysOffArray } = require('../src/actions/bookingDaysOffActions');
-        const startDate = new Date("2018-10-20");
-        const endDate = new Date("2018-10-10");
-        const input = createDaysOffArray(startDate, endDate);
-        const result = [];
-
-        chai.expect(input).to.deep.equal(result);
-        done();
+        const startDate = "2018-10-20";
+        const endDate = "2018-10-10";
+        createDaysOffArray(startDate, endDate)
+        .catch(error => {
+            chai.expect(error.message).to.deep.equal("End date lower than start date");
+            done();
+        })
     })
     
 })
