ae4a3d97438afe992d335882b9c39cb82d188720
ciprian24 <ciprian.sanudleanu@gmail.com>
2018-06-08 17:25:57 +0300

    started refactor with mongo

--- src/actions/retriveAlreadyBookedDaysActions.js
+++ src/actions/retriveAlreadyBookedDaysActions.js
@@ -1,39 +1,42 @@
-const { readFile, parseJSON } = require("../helpers/helpers");
+// const { readFile, parseJSON } = require("../helpers/helpers");
+    const DaysOff = require("../models/daysOffModel")
+ // const parseJSONFromFile = path => {
+//     return readFile(path)
+//         .then(parseJSON)
+//         .catch(err => {
+//             throw {
+//                 status: 500,
+//                 message: "the error was logged and we’ll be checking it shortly"
+//             };
+//         });
+// };
 
-const parseJSONFromFile = path => {
-    return readFile(path)
-        .then(parseJSON)
-        .catch(err => {
-            throw {
-                status: 500,
-                message: "the error was logged and we’ll be checking it shortly"
-            };
-        });
-};
-
-const findBookedDays = (userId, daysOff) => {
-    let daysBooked = [];
-    daysOff.forEach(element => {
-        if (element["userId"] === userId) {
-            daysBooked.push(...element["daysOff"]);
-        }
-    });
-    return daysBooked;
-};
+// const findBookedDays = (userId, daysOff) => {
+//     let daysBooked = [];
+//     daysOff.forEach(element => {
+//         if (element["userId"] === userId) {
+//             daysBooked.push(...element["daysOff"]);
+//         }
+//     });
+//     return daysBooked;
+// };
+const findBookedDays = (userId) => {
+    let findDaysOff = 
+}
 
 const getAlreadyBookedDays = (userId, daysOff) => {
     return new Promise((response, reject) => {
-        const bookedDays = findBookedDays(userId, daysOff);
+        const bookedDays = findBookedDays(userId, daysOff)
         if (bookedDays.length === 0)
             reject({
                 status: 404,
-                message: "User not found/no days booked yet"
-            });
+                message: 'User not found/no days booked yet',
+            })
         else {
-            response(bookedDays);
+            response(bookedDays)
         }
-    });
-};
+    })
+}
 
 const sendResponse = (request, response, bookedDays) => {
     return new Promise((resolve, reject) => {
@@ -43,35 +46,34 @@ const sendResponse = (request, response, bookedDays) => {
                 links: {
                     POST: `http://localhost:3000/days`,
                     DELETE: `http://localhost:3000/days/${Number(
-                        request.params.id
-                    )}`
-                }
-            });
-            resolve();
+                        request.params.id,
+                    )}`,
+                },
+            })
+            resolve()
         } catch (err) {
-            reject(err);
+            reject(err)
         }
-    });
-};
+    })
+}
 
 const sendError = (response, err) => {
-    response.status(err.status).json({ error: err.message });
-};
+    response.status(err.status).json({ error: err.message })
+}
 
 const retriveAlreadyBookedDays = (request, response) => {
-    parseJSONFromFile("db/daysOff.json")
-        .then(getAlreadyBookedDays.bind(null, Number(request.params.id)))
+    getAlreadyBookedDays(request.params.id)
         .then(sendResponse.bind(null, request, response))
-        .catch(sendError.bind(null, response));
-};
-if (process.env.NODE_ENV === "dev") {
+        .catch(sendError.bind(null, response))
+}
+if (process.env.NODE_ENV === 'dev') {
     module.exports = {
         retriveAlreadyBookedDays,
         findBookedDays,
-        getAlreadyBookedDays
-    };
+        getAlreadyBookedDays,
+    }
 } else {
     module.exports = {
-        retriveAlreadyBookedDays
-    };
+        retriveAlreadyBookedDays,
+    }
 }
