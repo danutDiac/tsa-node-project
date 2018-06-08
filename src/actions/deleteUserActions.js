24f79fa11b555bbd52519680d6fab84b5e508bfa
andradnl <andra.danalachi@gmail.com>
2018-06-08 12:42:28 +0300

    added delete user mongo tests

--- src/actions/deleteUserActions.js
+++ src/actions/deleteUserActions.js
@@ -1,21 +1,34 @@
 let User = require("../models/userModel");
+let mongoose = require("mongoose")
 
 const deleteUser = (req, res) => {
-    User.findByIdAndRemove(req.params.id)
-        .then(user => {
-            res.status(200).json({
-                deletedUser: user,
-                links: {
-                    "GET": req.headers.host + req.baseUrl,
-                    "POST": req.headers.host + req.baseUrl
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(400).json({
+            message: "Bad request"
+        })
+    } else {
+        User.findByIdAndRemove(req.params.id)
+            .then(user => {
+                if (!user) {
+                    res.status(404).json({
+                        message: "User not found"
+                    })
+                } else {
+                    res.status(200).json({
+                        deletedUser: user,
+                        links: {
+                            "GET": req.headers.host + req.baseUrl,
+                            "POST": req.headers.host + req.baseUrl
+                        }
+                    })
                 }
             })
-        })
-        .catch(err => {
-            res.status(500).json({
-                serverErrorMessage: "the error was logged and we’ll be checking it shortly"
+            .catch(err => {
+                res.status(500).json({
+                    serverErrorMessage: "the error was logged and we’ll be checking it shortly"
+                })
             })
-        })
+    }
 }
 
 if (process.env.NODE_ENV == "dev") {
--- tests/deleteUserRoutes.test.js
+++ tests/deleteUserRoutes.test.js
@@ -1,51 +1,52 @@
+const mongoose = require("mongoose")
+const config = require("../config/config")
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const fs = require('fs')
 const server = require('../src/main')
-const { writeFile } = require('../src/helpers/helpers')
 const should = chai.should()
 chai.use(chaiHttp)
 
+let User = require("../src/models/userModel")
+
 describe('DELETE /users/:id', function () {
+    let id;
     beforeEach(function (done) {
-        const users = [
-            {
-                "id": 0,
-                "firstName": "",
-                "lastName": "",
-                "email": "",
-                "phone": ""
-            }
-        ]
-        
-        writeFile("db/users.json", JSON.stringify(users))
-        .then(() => {
-            done();
+        mongoose.connect(config.mongoUrl).then(() => {
+            mongoose.connection.db.dropDatabase()
+            let newUser = new User({
+                "firstName": "Flow",
+                "lastName": "Bamboozle",
+                "email": "flo@w.com",
+                "phone": "0749666666"
+            })
+            newUser.save().then(user => {
+                id = user._id
+                done()
+            })
+                .catch(err => {
+                    done()
+                });
         })
     })
-    it('should return status 404 for id not found', function (done) {
-        chai.request(server).delete('/users/-1')
-            .end(function (err, res) {
-                res.should.have.status(404)
-                res.should.be.json
-                res.body.message.should.equal('User not found')
-                done()
-            })
-    })
-    it('should return status 404 for invalid id', function (done) {
-        chai.request(server).delete('/users/A')
-            .end(function (err, res) {
-                res.should.have.status(404)
-                res.should.be.json
-                res.body.message.should.equal('User not found')
-                done()
-            })
-    })
-    it('should return status 200 for id found', function (done) {
-        chai.request(server).delete('/users/0')
+    it("should return status 200 for id found", function (done) {
+        chai.request(server).delete(`/users/${id}`)
             .end(function (err, res) {
                 res.should.have.status(200)
                 done()
             })
+    })
+    it("should return 404 if id not found", function(done) {
+        chai.request(server).delete(`/users/123456789abv`)
+        .end(function(err, res) {
+            res.should.have.status(404)
+            done()
+        })
+    })
+    it.only("should return 400 for invalid id", function(done) {
+        chai.request(server).delete(`/users/123456789ab`)
+        .end(function(err, res) {
+            res.should.have.status(400)
+            done()
+        })
     })
 })
