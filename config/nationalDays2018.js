78561d0461619461871414cdd98ac1f2cd79fe1c
h-mihail <33.mihail@gmail.com>
2018-06-12 12:15:25 +0300

    auto-populate with national days on start

---
+++ config/nationalDays2018.js
@@ -0,0 +1,41 @@
+module.exports = [{
+    "name": "Anul nou",
+    "days": ["2018-01-01", "2018-01-02"]
+},
+{
+    "name": "Ziua Unirii Principatelor Romane",
+    "days": ["2018-01-24"]
+},
+{
+    "name": "Sarbatorile Pascale",
+    "days": ["2018-04-08", "2018-04-09"]
+},
+{
+    "name": "Ziua Muncii",
+    "days": ["2018-05-01"]
+},
+{
+    "name": "Rusaliile",
+    "days": ["2018-05-27", "2018-05-28"]
+},
+{
+    "name": "Ziua Copilului",
+    "days": ["2018-06-01"]
+},
+{
+    "name": "Adormirea Maicii Domnului",
+    "days": ["2018-08-15"]
+},
+{
+    "name": "Sfantul Andrei",
+    "days": ["2018-11-30"]
+},
+{
+    "name": "Ziua Nationala a Romaniei",
+    "days": ["2018-12-01"]
+},
+{
+    "name": "Craciunul",
+    "days": ["2018-12-25", "2018-12-26"]
+}
+]
