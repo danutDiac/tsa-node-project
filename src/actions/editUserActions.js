6871f72ccbef14e6e2394b02d5bdbe4557eb1a0b
MihaiBojescu <bojescu.mihai@gmail.com>
2018-06-15 15:27:33 +0300

    Refactored actions to use chain of promises

--- src/actions/editUserActions.js
+++ src/actions/editUserActions.js
@@ -1,54 +1,52 @@
 let User = require("../models/userModel");
 
 const editUserPut = (req, res) => {
-    if(Object.keys(req.body).length===4){
-    User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }).then(user => {
-        reqValidData(req.body, user, 1)
-            .then(() => res.status(200).json({
-                updatedUser: user,
-                links: {
-                    "GET": req.headers.host + req.originalUrl,
-                    "PUT": req.headers.host + req.originalUrl,
-                    "DELETE": req.headers.host + req.originalUrl,
-                    "POST": req.headers.host + req.baseUrl
-                }
-
-            }))
-            .catch(err => {
-                res.status(err["status"]).json({ "message": err["message"] })
-            })
-    }).catch(err => {
-        res.status(500).json({
-            serverErrorMessage: "the error was logged and we’ll be checking it shortly"
+    if (Object.keys(req.body).length === 4) {
+        User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {
+            new: true
         })
-    })
-         }       else{
-            res.status(500).json({
-                serverErrorMessage: "need more parameters"
-            })
-                    }
+        .then(reqValidData.bind(null,req.body,1))
+        .then(sendResponse.bind(null, req, res))
+        .catch(err => {
+            if (typeof err === "object") {
+                res.status(err["status"]).json({
+                    "message": err["message"]
+                })
+            }
+            else {
+                res.status(500).json({
+                    serverErrorMessage: "the error was logged and we’ll be checking it shortly"
+                })
+            }
+        })
+    } else {
+        res.status(500).json({
+            serverErrorMessage: "need more parameters"
+        })
+    }
 }
 
 const editUserPatch = (req, res) => {
-    User.findByIdAndUpdate(req.params.id, { $set: req.body },{ new: true }).then(user => {
-            reqValidData(req.body, user, 0)
-                .then(() => res.status(200).json({
-                    updatedUser: user,
-                    links: {
-                        "GET": req.headers.host + req.originalUrl,
-                        "PUT": req.headers.host + req.originalUrl,
-                        "DELETE": req.headers.host + req.originalUrl,
-                        "POST": req.headers.host + req.baseUrl
-                    }
-                }))
-                .catch(err => {
-                    res.status(err["status"]).json({ "message": err["message"] })
-                })
-            })
+    User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {
+            new: true
+        })
+        .then(reqValidData.bind(null, req.body, 0))
+        .then(sendResponse.bind(null, req, res))
         .catch(err => {
-            res.status(500).json({
-                serverErrorMessage: "the error was logged and we’ll be checking it shortly"
-            })
+            if (typeof err === "object") {
+                res.status(err["status"]).json({
+                    "message": err["message"]
+                })
+            }
+            else {
+                res.status(500).json({
+                    serverErrorMessage: "the error was logged and we’ll be checking it shortly"
+                })
+            }
         })
 }
 
@@ -60,7 +58,7 @@ let checkMail = (newMail, allMails) => {
     return 0;
 };
 
-let reqValidData = (body, users, putMethod) => {
+let reqValidData = (body,putMethod,users) => {
     //putMethod=1 daca aplicam metoda pe put
     //putMethod=0 daca apliam metodaspe patch
     return new Promise((resolve, reject) => {
@@ -69,16 +67,14 @@ let reqValidData = (body, users, putMethod) => {
         if (body.firstName) {
             if (!/^[a-zA-Z]+$/.test(body.firstName))
                 error += "Ati introdus prenumele gresit" + "\n";
-        }
-        else if (putMethod) {
+        } else if (putMethod) {
             error += "Nu ati introdus niciun prenume. \n"
         }
 
         if (body.lastName) {
             if (!/^[a-zA-Z]+$/.test(body.lastName))
                 error += "Ati introdus numele gresit" + "\n";
-        }
-        else if (putMethod) {
+        } else if (putMethod) {
             error += "Nu ati introdus niciun nume. \n"
         }
 
@@ -91,24 +87,20 @@ let reqValidData = (body, users, putMethod) => {
             if (checkMail(body.email, users) === 1) {
                 error += "Mailul folosit exista deja in baza de date";
             }
-        }
-        else if (putMethod) {
+        } else if (putMethod) {
             error += "Nu ati introdus niciun email \n"
         }
 
         if (body.phone) {
             if (!/^([0]{1})\d{5,9}$/.test(body.phone))
                 error += "Ati introdus gresit numarul de telefon";
-        }
-        else if (putMethod) {
+        } else if (putMethod) {
             error += "Nu ati introdus niciun numar de telefon \n"
         }
 
         if (error === "") {
-            resolve();
-        }
-
-        else {
+            resolve(users);
+        } else {
             reject({
                 "status": 400,
                 "message": error
@@ -116,6 +108,18 @@ let reqValidData = (body, users, putMethod) => {
         }
     })
 };
+
+const sendResponse = (req, res, user) => {
+    res.status(200).json({
+        updatedUser: user,
+        links: {
+            "GET": req.headers.host + req.originalUrl,
+            "PUT": req.headers.host + req.originalUrl,
+            "DELETE": req.headers.host + req.originalUrl,
+            "POST": req.headers.host + req.baseUrl
+        }
+    })
+}
 
 if (process.env.NODE_ENV == "dev") {
     module.exports = {
